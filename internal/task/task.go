645d1ace60cdcc01509f588b77bdd3943e5b851e
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-10-01 00:02:46 +0100

    Add task state

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,1 +1,15 @@
+/*
+Task contains everything needed by workers to execute and manage a workload.
+*/
 package task
+
+// State describes a task at a specific moment in time.
+type State byte
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Failed
+	Completed
+)
