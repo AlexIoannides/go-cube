c490e96027506ed3a6165ea2954914435c019913
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-10-02 23:39:35 +0100

    Add skeleton code for internal package

--- internal/task/task.go
+++ internal/task/task.go
@@ -3,6 +3,13 @@ Task contains everything needed by workers to execute and manage a workload.
 */
 package task
 
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
 // State describes a task at a specific moment in time.
 type State byte
 
@@ -13,3 +20,26 @@ const (
 	Failed
 	Completed
 )
+
+// Task holds all information about a task scheduled on a Worker.
+type Task struct {
+	Id            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+// TaskEvent represents desired transition states for scheduled tasks
+type TaskEvent struct {
+	Id        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
+}
